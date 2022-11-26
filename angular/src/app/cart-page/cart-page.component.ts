71ba628901278abae31bbd77c38a0f38e58a81fa
MSI\zackt <ctse48@my.centennialcollege.ca>
2022-11-25 19:14:37 -0500

    Created the cart page

---
+++ angular/src/app/cart-page/cart-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../services/item.service';
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.scss']
+})
+export class CartPageComponent implements OnInit {
+
+  itemInCart:any;
+
+  constructor(private itemAPIs:ItemService) { }
+
+  ngOnInit(): void {
+    
+    this.itemInCart = this.itemAPIs.getItemsInCart();
+    console.log("itemInCart:",this.itemInCart)
+  }
+
+  removeItemInCart(selected:any){
+    console.log("selected:",selected._id)
+    this.itemAPIs.removeItem(selected._id);
+    this.itemInCart =  this.itemInCart.filter((item: any)  => item._id !== selected._id );
+
+  }
+}
